4bcc0d185324cb10090e07481174a15905982736
Sebastien Modley <sebmodley@gmail.com>
2023-11-23 16:29:02 +0000

    Add test project

---
+++ GenericList_Test/GenericList_Test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace GenericListTest
+{
+	TEST_CLASS(GenericListTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
